06ed11da9f27e718486a2e54b1c05412633456e5
Beilak <beylak@yandex.ru>
2024-01-02 14:06:02 +0300

    add risk protocol

--- src/protocols/risk/risk.py
+++ src/protocols/risk/risk.py
@@ -2,4 +2,11 @@ import typing as tp
 
 
 class RiskProtocol(tp.Protocol):
-    ...
+    def is_account_risk_realized(self) -> bool:
+        """Checker provide True if risk for account has been relized"""
+
+    def is_session_risk_realized(self) -> bool:
+        """Checker provide True if risk for session (traiding day) has been relized"""
+
+    def is_securities_risk_realized(self) -> bool:
+        """Checker provide True if risk for securities has been relized"""
